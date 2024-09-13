3f55a2a97d3e0be77ed9c767120f94c2e4fd4d39
JohnBeskow <johnbeskow.jb@gmail.com>
2024-09-13 03:09:06 +0200

    just needs to make esp-idf library with nimble to work with esp32c6

--- main/main.c
+++ main/main.c
@@ -31,9 +31,6 @@ static int device_read(uint16_t conn_handle, uint16_t attr_handle, struct ble_ga
 }
 
 
-
-
-
 //services and characteristics read/write data functions
 static const struct ble_gatt_svc_def gatt_svcs[] = {
     {
@@ -48,9 +45,63 @@ static const struct ble_gatt_svc_def gatt_svcs[] = {
             .access_cb = device_write}, // function called when accessed
             {0}
         }
-    }
+    },
 {0}
 };
+
+
+static int ble_gap_event(struct ble_gap_event *event , void *arg)
+{
+    switch (event->type)
+    {
+        //advertise if connected
+        case BLE_GAP_EVENT_CONNECT:
+            ESP_LOGI("GAP", "BLE GAP EVENT CONNECT %s ", event->connect.status == 0 ? "OK!" : "FAILED!");
+            if (event->connect.status != 0)
+            {
+                ble_app_advertise();
+            }
+            break;
+        //advertise again after disconnect
+        case BLE_GAP_EVENT_DISCONNECT:
+            ESP_LOGI("GAP", "BLE GAP EVENT DISCONNECTED %s ", event->disconnect.reason == 0 ? "OK!" : "FAILED!");
+            break;
+        case BLE_GAP_EVENT_ADV_COMPLETE:
+            ESP_LOGI("GAP", "BLE GAP EVENT ADV COMPLETE %s ", event->adv_complete.reason == 0? "OK!" : "FAILED!");
+            break;
+        default:    
+            break;
+    }
+    return 0;
+}
+
+// defines connections
+void ble_app_advertise(void)
+{
+    // GAP device name definition 
+    struct ble_hs_adv_fields fields;            //fields for advertising
+    const char *device_name;
+    memset(&fields, 0, sizeof(fields));         //sets fields to 0
+    device_name = ble_svc_gap_device_name();    //get device name
+    fields.name = (uint8_t *)device_name;       //set name
+    fields.name_len = strlen(device_name);      //set name length
+    ble_gap_adv_set_fields(&fields);            //set fields
+
+    // GAP connectivity definition (advertising)
+    struct ble_gap_adv_params adv_params;       //advertising parameters
+    memset(&adv_params, 0, sizeof(adv_params)); //sets adv_params to 0
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;   //undirected connectable mode
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;   //general discoverable mode
+    ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event, NULL); //start advertising
+
+}
+
+
+void ble_app_on_sync(void)
+{
+    ble_hs_id_infer_auto(0, &ble_addr_type);    //determines best address auto
+    ble_app_advertise();                        //defines connection
+}
 
 void app_main(void)
 {
@@ -61,10 +112,11 @@ void app_main(void)
     //initialize Nimble Config:
     nimble_port_init();                         //initialize nimble host
     ble_svc_gap_device_name_set("BleServer");   //set device name
-    ble_svc_gap_init();                         //initialize gap service
-    ble_svc_gatt_init();                        //initialize gatt service
-    ble_gatts_count_cfg(gatt_svcs);
-
+    ble_svc_gap_init();                         //comms
+    ble_svc_gatt_init();                        //data organization
+    ble_gatts_count_cfg(gatt_svcs);             //count the number of services
+    ble_gatts_add_svcs(gatt_svcs);
+    ble_hs_cfg.sync_cb = ble_app_on_sync;
 
 
 
